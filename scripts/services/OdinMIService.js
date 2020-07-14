7eadb4a7171645a996f946e9ca14297d002d82ef
Kathiravanpj <Kathiravan.chandran@leanswift.com>
2020-07-14 12:25:28 -0400

    Pcs260

---
+++ scripts/services/OdinMIService.js
@@ -0,0 +1,27 @@
+/**
+ * Utility service to execute M3 MI transactions using built in MI service
+ */
+var h5;
+(function (h5) {
+    var application;
+    (function (application) {
+        var OdinMIService = /** @class */ (function () {
+            function OdinMIService(miService) {
+                this.miService = miService;
+            }
+            OdinMIService.prototype.callWebService = function (program, transaction, requestData, maxReturnedRecords, company, division) {
+                if (maxReturnedRecords === void 0) { maxReturnedRecords = 100; }
+                var request = {
+                    program: program,
+                    transaction: transaction,
+                    record: requestData,
+                    maxReturnedRecords: maxReturnedRecords
+                };
+                return this.miService.executeRequest(request).then(function (val) { return val; });
+            };
+            OdinMIService.$inject = ["m3MIService"];
+            return OdinMIService;
+        }());
+        application.OdinMIService = OdinMIService;
+    })(application = h5.application || (h5.application = {}));
+})(h5 || (h5 = {}));
